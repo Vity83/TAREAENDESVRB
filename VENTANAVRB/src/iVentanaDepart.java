3f09ab08cbc2d4aee186371d9bf2862a5452fc69
Vity83 <100796720+Vity83@users.noreply.github.com>
2022-03-12 21:12:19 +0100

    Tarea de entornos de desarrollo Alumno VRB
    El programa no funciona una vez hechos los cambios , en mi opinion es porque no sé como hacer correctamente el apartado de INLINE

---
+++ VENTANAVRB/src/iVentanaDepart.java
@@ -0,0 +1,11 @@
+import java.awt.event.ActionEvent;
+
+public interface iVentanaDepart {
+
+	void modificacion(ActionEvent e, String NOEXISTE_DEPART);
+
+	void baja(ActionEvent e, String NOEXISTE_DEPART);
+
+	void alta(ActionEvent e);
+
+}
